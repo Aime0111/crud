f39f9901da04964cb301bde717023d3c9b90a7d9
Aime <aimeca011.com>
2025-02-19 11:58:40 -0600

    Actualización bd alumnos en app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return "Hola Mundo"
+
+# Definición del modelo de la tabla 'estudiantes'
+class Alumno(db.Model):
+    __tablename__ = 'alumnos'
+    no_control = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String)
+    ap_paterno = db.Column(db.String)
+    ap_materno = db.Column(db.String)
+    semestre = db.Column(db.Integer)
